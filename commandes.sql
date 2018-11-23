0fc1f96eec9e2cd30823e7d22fbc23ef2b240f0f
JordanFist <jsandri@skeleton.pedago.ipb.fr>
2018-11-23 15:23:00 +0100

    ok

--- commandes.sql
+++ commandes.sql
@@ -4,6 +4,21 @@ from PERSONNES
 natural join ADHESIONS natural join ASSOCIATIONS natural join ADHERENTS
 where NOM_ASSOCIATION = 'BDE' and DATE_ADHESION > '10-JUN-10' and COTISATION_ADHERENT > 0;
 
--- 2.La liste des personnes organisant un évènement et celles des personnes participant à cet évènement
+-- 2.La liste des personnes participant à un évènement 
 select NOM_PERSONNE, PRENOM_PERSONNE
-from PERSONNE
+from PERSONNES
+natural join PARTICIPATIONS natural join EVENEMENTS
+where TITRE = 'GALA';
+
+--3. La liste des personnes organisant à un évènement
+select NOM_PERSONNE, PRENOM_PERSONNE
+from PERSONNES
+natural join ADHERENTS natural join GESTIONS natural join EVENEMENTS
+where TITRE = 'OLINP';
+
+--4. Savoir le nombre de commentaires postés pour une news
+--TOFIX
+select TITRE_NEWS, count(NUMERO_ADHERENT) as NOMBRE
+from NEWS
+natural join COMMENTAIRES group by TITRE_NEWS;
+
