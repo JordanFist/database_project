3490bee0aa8fb05834ea2892f36ba8e56fc61649
JordanFist <jsandri@amnesix.pedago.ipb.fr>
2018-11-30 15:58:36 +0100

    ok

--- base.sql
+++ base.sql
@@ -92,10 +92,11 @@ create table ADHESIONS
 create table NEWS
 (
     NUMERO_NEWS			NUMBER(3)		not null,
+    NUMERO_ADHERENT		NUMBER(3)			,
     TITRE_NEWS			CHAR(30)		not null,
     TEXTE_NEWS			VARCHAR2(100)			,
     DATE_DE_CREATION_NEWS	DATE				,
-    NUMERO_ADHERENT		CHAR(20)			,
+    
     constraint pk_news primary key (NUMERO_NEWS)
 );
 
