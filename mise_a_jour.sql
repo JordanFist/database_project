071505be6343af7d8b411948f7598808d87ee3af
JordanFist <jsandri@jbay.pedago.ipb.fr>
2018-12-06 14:01:32 +0100

    ok

--- mise_a_jour.sql
+++ mise_a_jour.sql
@@ -1,32 +1,95 @@
 -- Ajout d'un adhérent
-INSERT INTO ADHERENTS (NUMERO_ADHERENT, NUMERO_PERSONNE)
-VALUES (11, 11);
+insert into ADHERENTS (NUMERO_ADHERENT, NUMERO_PERSONNE)
+values (11, 11);
 
--- Suppression d'un adhérent
+-- Suppression d'un adhérent 
+delete from NOTATIONS
+where NUMERO_ADHERENT in (
+    select NUMERO_ADHERENT
+    from ADHERENTS
+    where NUMERO_PERSONNE = 8
+);
+
+delete from GESTIONS
+where NUMERO_ADHERENT in (
+    select NUMERO_ADHERENT
+    from ADHERENTS
+    where NUMERO_PERSONNE = 8
+);
+
+delete from COMMENTAIRES
+where NUMERO_ADHERENT in (
+    select NUMERO_ADHERENT
+    from ADHERENTS
+    where NUMERO_PERSONNE = 8
+);
+
+delete from ADHESIONS
+where NUMERO_ADHERENT in (
+    select NUMERO_ADHERENT
+    from ADHERENTS
+    where NUMERO_PERSONNE = 8
+);
+
+delete from ADHERENTS
+where NUMERO_PERSONNE = 8;
 
 
 -- Modification d'un adhérent
-UPDATE ADHERENTS
-SET NUMERO_PERSONNE = 3 where NUMERO_PERSONNE = 2; 
+update ADHERENTS
+set NUMERO_PERSONNE = 3 where NUMERO_PERSONNE = 2; 
 
--- Ajout d'un évènement
-INSERT INTO EVENEMENTS (NUMERO_EVENEMENT, TITRE, DESCRIPTION, COUT, LIEU, NOMBRE_DE_PLACES)
-VALUES (11, "TEST", "SUPER", 42, "MARSEILLE", 50);
 
--- Suppression d'un évènement
 
+-- Ajout d'un évènement  
+insert into EVENEMENTS (NUMERO_EVENEMENT, TITRE, DESCRIPTION, COUT, LIEU, NOMBRE_DE_PLACES)
+values (15, 'TEST', 'SUPER', 42, 'MARSEILLE', 50);
+
+-- Suppression d'un évènement 
+delete from PARTICIPATIONS
+where NUMERO_EVENEMENT in (
+    select NUMERO_EVENEMENT
+    from EVENEMENTS
+    where TITRE = 'GALA'
+);
+
+delete from NOTATIONS
+where NUMERO_EVENEMENT in (
+    select NUMERO_EVENEMENT
+    from EVENEMENTS
+    where TITRE = 'GALA'
+);
+
+delete from GESTIONS
+where NUMERO_EVENEMENT in (
+    select NUMERO_EVENEMENT
+    from EVENEMENTS
+    where TITRE = 'GALA'
+);
+
+delete from ORGANISATIONS
+where NUMERO_EVENEMENT in (
+    select NUMERO_EVENEMENT
+    from EVENEMENTS
+    where TITRE = 'GALA'
+);
+
+delete from EVENEMENTS
+where TITRE = 'GALA';
 
 -- Modification d'un évènement
-UPDATE EVENEMENTS
-SET COUT = COUT + 1; 
+update EVENEMENTS
+set COUT = COUT + 1; 
 
--- Ajout d'une association
-INSERT INTO ASSOCIATIONS (NUMERO_ASSOCIATION, NOM_ASSOCIATION, DATE_DE_CREATION, DESCRIPTION)
-VALUES (11, "GCC", "22-JAN-16", "Blablabla");
 
--- Suppresion d'une association
+
+-- Ajout d'une association  
+insert into ASSOCIATIONS (NUMERO_ASSOCIATION, NOM_ASSOCIATION, DATE_DE_CREATION, DESCRIPTION)
+values (11, 'GCC', '22-JAN-16', 'Blablabla');
+
+-- Suppresion d'une association   TOFIX
 
 
 -- Modification d'une association
-UPDATE ASSOCIATIONS
-SET NUMERO_ASSOCIATION = NUMERO_ASSOCIATION + 1;
+update ASSOCIATIONS
+set DATE_DE_CREATION = '26-JUN-20' where DATE_DE_CREATION = '25-JUN-20';
