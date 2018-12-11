048d62af60471f73d8b88061e4319de6ae128e7e
Sarah Peris <speris@enseirb-matmeca.fr>
2018-12-11 23:50:36 +0100

    consult

--- oracle/Consultations.java
+++ oracle/Consultations.java
@@ -40,8 +40,8 @@ public class Consultations {
             conn = ods.getConnection();
             stmt = conn.createStatement();
             rset = stmt.executeQuery(
-                    "select NOM_PERSONNE, PRENOM_PERSONNE from PERSONNES natural join ADHERENTS natural join ADHESIONS natural join ASSOCIATIONS where NOM_ASSOCIATION = '"
-                            + association + "'");
+				     "select NOM_PERSONNE, PRENOM_PERSONNE from PERSONNES natural join ADHERENTS natural join ADHESIONS natural join ASSOCIATIONS where NOM_ASSOCIATION = '"
+				     + association + "'");
 
             while (rset.next()) { // Affichage du resultat.
                 System.out.println(rset.getString(2) + rset.getString(1));
@@ -58,8 +58,8 @@ public class Consultations {
             conn = ods.getConnection();
             stmt = conn.createStatement();
             ResultSet rset = stmt.executeQuery(
-                    "select NOM_PERSONNE, PRENOM_PERSONNE from PERSONNES natural join ADHERENTS natural join ADHESIONS natural join ASSOCIATIONS where NOM_ASSOCIATION = '"
-                            + association + "' and DATE_ADHESION > '" + date + "'");
+					       "select NOM_PERSONNE, PRENOM_PERSONNE from PERSONNES natural join ADHERENTS natural join ADHESIONS natural join ASSOCIATIONS where NOM_ASSOCIATION = '"
+					       + association + "' and DATE_ADHESION > '" + date + "'");
 
             while (rset.next()) { // Affichage du resultat.
                 System.out.println(rset.getString(2) + rset.getString(1));
@@ -76,8 +76,8 @@ public class Consultations {
             conn = ods.getConnection();
             stmt = conn.createStatement();
             ResultSet rset = stmt.executeQuery(
-                    "select NOM_PERSONNE, PRENOM_PERSONNE from PERSONNES natural join ADHERENTS natural join ADHESIONS natural join ASSOCIATIONS where NOM_ASSOCIATION = '"
-                            + association + "' and DATE_ADHESION > '" + date + "' and COTISATION_ADHERENT = 0");
+					       "select NOM_PERSONNE, PRENOM_PERSONNE from PERSONNES natural join ADHERENTS natural join ADHESIONS natural join ASSOCIATIONS where NOM_ASSOCIATION = '"
+					       + association + "' and DATE_ADHESION > '" + date + "' and COTISATION_ADHERENT = 0");
 
             while (rset.next()) { // Affichage du resultat.
                 System.out.println(rset.getString(2) + rset.getString(1));
@@ -94,8 +94,8 @@ public class Consultations {
             conn = ods.getConnection();
             stmt = conn.createStatement();
             ResultSet rset = stmt.executeQuery(
-                    "select NOM_PERSONNE, PRENOM_PERSONNE from PERSONNES natural join PARTICIPATIONS natural join EVENEMENTS where TITRE = '"
-                            + evenement + "'");
+					       "select NOM_PERSONNE, PRENOM_PERSONNE from PERSONNES natural join PARTICIPATIONS natural join EVENEMENTS where TITRE = '"
+					       + evenement + "'");
 
             while (rset.next()) { // Affichage du resultat.
                 System.out.println(rset.getString(2) + rset.getString(1));
@@ -112,8 +112,8 @@ public class Consultations {
             conn = ods.getConnection();
             stmt = conn.createStatement();
             ResultSet rset = stmt.executeQuery(
-                    "select NOM_PERSONNE, PRENOM_PERSONNE from PERSONNES natural join ADHERENTS natural join GESTIONS natural join EVENEMENTS where TITRE = '"
-                            + evenement + "'");
+					       "select NOM_PERSONNE, PRENOM_PERSONNE from PERSONNES natural join ADHERENTS natural join GESTIONS natural join EVENEMENTS where TITRE = '"
+					       + evenement + "'");
 
             while (rset.next()) { // Affichage du resultat.
                 System.out.println(rset.getString(2) + rset.getString(1));
@@ -130,8 +130,8 @@ public class Consultations {
             conn = ods.getConnection();
             stmt = conn.createStatement();
             ResultSet rset = stmt.executeQuery(
-                    "select TITRE_NEWS, count(*) as NOMBRE from NEWS join COMMENTAIRES on NEWS.NUMERO_NEWS = COMMENTAIRES.NUMERO_NEWS where TITRE_NEWS = '"
-                            + news + "' group by TITRE_NEWS");
+					       "select TITRE_NEWS, count(*) as NOMBRE from NEWS join COMMENTAIRES on NEWS.NUMERO_NEWS = COMMENTAIRES.NUMERO_NEWS where TITRE_NEWS = '"
+					       + news + "' group by TITRE_NEWS");
 
             while (rset.next()) { // Affichage du resultat.
                 System.out.println(rset.getString(2));
@@ -140,4 +140,39 @@ public class Consultations {
             closeBase();
         }
     }
+
+
+    void choix7() throws SQLException, ClassNotFoundException, java.io.IOException {
+        openBase();
+
+        try {
+            conn = ods.getConnection();
+            stmt = conn.createStatement();
+            ResultSet rset = stmt.executeQuery("select NOM_ASSOCIATION, NUMERO_ASSOCIATION from ASSOCIATIONS");
+
+            while (rset.next()) { // Affichage du resultat.
+                System.out.println(rset.getString(2) + "\t" + rset.getString(1));
+            }
+        } finally {
+            closeBase();
+        }
+    }
+
+
+    void choix8() throws SQLException, ClassNotFoundException, java.io.IOException {
+        openBase();
+
+        try {
+            conn = ods.getConnection();
+            stmt = conn.createStatement();
+            ResultSet rset = stmt.executeQuery("select NUMERO_EVENEMENT, TITRE from EVENEMENTS");
+
+            while (rset.next()) { // Affichage du resultat.
+                System.out.println(rset.getString(1)+ "\t" + rset.getString(2));
+            }
+        } finally {
+            closeBase();
+        }
+    }
+
 }
