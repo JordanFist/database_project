711d6003da8dad599d6eca1856d0b2b120caa5a0
JordanFist <jsandri@botvinnik.pedago.ipb.fr>
2018-12-11 20:23:38 +0100

    ok

--- oracle/Consultations.java
+++ oracle/Consultations.java
@@ -21,7 +21,31 @@ public class Consultations {
         }
     }
 
-    void choix1(String assosciation) throws SQLException, ClassNotFoundException, java.io.IOException {
+    void choix1(String association) throws SQLException, ClassNotFoundException, java.io.IOException {
+        ods = new OracleDataSource();
+        ods.setUser("jsandri");
+        ods.setPassword("jsandri");
+        ods.setURL("jdbc:oracle:thin:@localhost:1521/oracle");
+
+        conn = null;
+        stmt = null;
+
+	ResultSet rset = null;
+
+        try {
+            conn = ods.getConnection();
+            stmt = conn.createStatement();
+            rset = stmt.executeQuery("select NOM_PERSONNE, PRENOM_PERSONNE from PERSONNES natural join ADHERENTS natural join ADHESIONS natural join ASSOCIATIONS where NOM_ASSOCIATION = '" + association + "'");
+
+            while (rset.next()) { // Affichage du resultat.
+		System.out.println(rset.getString(2) + rset.getString(1));
+            }
+        } finally {
+            closeBase();
+        }
+    }
+
+    void choix2(String association, String date) throws SQLException, ClassNotFoundException, java.io.IOException {
         ods = new OracleDataSource();
         ods.setUser("jsandri");
         ods.setPassword("jsandri");
@@ -33,7 +57,95 @@ public class Consultations {
         try {
             conn = ods.getConnection();
             stmt = conn.createStatement();
-            ResultSet rset = stmt.executeQuery("select *" + "from ASSOCIATIONS");
+            ResultSet rset = stmt.executeQuery("select NOM_PERSONNE, PRENOM_PERSONNE from PERSONNES natural join ADHERENTS natural join ADHESIONS natural join ASSOCIATIONS where NOM_ASSOCIATION = '" + association + "' and DATE_ADHESION > '" + date + "'");
+
+            while (rset.next()) { // Affichage du resultat.
+                System.out.println(rset.getString(2) + rset.getString(1));
+            }
+        } finally {
+            closeBase();
+        }
+    }
+
+    void choix3(String association, String date) throws SQLException, ClassNotFoundException, java.io.IOException {
+        ods = new OracleDataSource();
+        ods.setUser("jsandri");
+        ods.setPassword("jsandri");
+        ods.setURL("jdbc:oracle:thin:@localhost:1521/oracle");
+
+        conn = null;
+        stmt = null;
+
+        try {
+            conn = ods.getConnection();
+            stmt = conn.createStatement();
+            ResultSet rset = stmt.executeQuery("select NOM_PERSONNE, PRENOM_PERSONNE from PERSONNES natural join ADHERENTS natural join ADHESIONS natural join ASSOCIATIONS where NOM_ASSOCIATION = '" + association + "' and DATE_ADHESION > '" + date + "' and COTISATION_ADHERENT = 0");
+
+            while (rset.next()) { // Affichage du resultat.
+                System.out.println(rset.getString(2) + rset.getString(1));
+            }
+        } finally {
+            closeBase();
+        }
+    }
+
+    void choix4(String evenement) throws SQLException, ClassNotFoundException, java.io.IOException {
+        ods = new OracleDataSource();
+        ods.setUser("jsandri");
+        ods.setPassword("jsandri");
+        ods.setURL("jdbc:oracle:thin:@localhost:1521/oracle");
+
+        conn = null;
+        stmt = null;
+
+        try {
+            conn = ods.getConnection();
+            stmt = conn.createStatement();
+            ResultSet rset = stmt.executeQuery("select NOM_PERSONNE, PRENOM_PERSONNE from PERSONNES natural join PARTICIPATIONS natural join EVENEMENTS where TITRE = '" + evenement + "'");
+
+            while (rset.next()) { // Affichage du resultat.
+                System.out.println(rset.getString(2) + rset.getString(1));
+            }
+        } finally {
+            closeBase();
+        }
+    }
+
+    void choix5(String evenement) throws SQLException, ClassNotFoundException, java.io.IOException {
+        ods = new OracleDataSource();
+        ods.setUser("jsandri");
+        ods.setPassword("jsandri");
+        ods.setURL("jdbc:oracle:thin:@localhost:1521/oracle");
+
+        conn = null;
+        stmt = null;
+
+        try {
+            conn = ods.getConnection();
+            stmt = conn.createStatement();
+            ResultSet rset = stmt.executeQuery("select NOM_PERSONNE, PRENOM_PERSONNE from PERSONNES natural join ADHERENTS natural join GESTIONS natural join EVENEMENTS where TITRE = '" + evenement + "'");
+
+            while (rset.next()) { // Affichage du resultat.
+                System.out.println(rset.getString(2) + rset.getString(1));
+            }
+        } finally {
+            closeBase();
+        }
+    }
+
+    void choix6(String news) throws SQLException, ClassNotFoundException, java.io.IOException {
+        ods = new OracleDataSource();
+        ods.setUser("jsandri");
+        ods.setPassword("jsandri");
+        ods.setURL("jdbc:oracle:thin:@localhost:1521/oracle");
+
+        conn = null;
+        stmt = null;
+
+        try {
+            conn = ods.getConnection();
+            stmt = conn.createStatement();
+            ResultSet rset = stmt.executeQuery("select TITRE_NEWS, count(*) as NOMBRE from NEWS join COMMENTAIRES on NEWS.NUMERO_NEWS = COMMENTAIRES.NUMERO_NEWS where TITRE_NEWS = '" + news + "' group by TITRE_NEWS");
 
             while (rset.next()) { // Affichage du resultat.
                 System.out.println(rset.getString(2));
@@ -41,20 +153,5 @@ public class Consultations {
         } finally {
             closeBase();
         }
-    }
-
-    void choix2(String assosciation, String date) throws SQLException, ClassNotFoundException, java.io.IOException {
-    }
-
-    void choix3(String assosciation, String date) throws SQLException, ClassNotFoundException, java.io.IOException {
-    }
-
-    void choix4(String assosciation) throws SQLException, ClassNotFoundException, java.io.IOException {
-    }
-
-    void choix5(String assosciation) throws SQLException, ClassNotFoundException, java.io.IOException {
-    }
-
-    void choix6(String assosciation) throws SQLException, ClassNotFoundException, java.io.IOException {
     }
 }
